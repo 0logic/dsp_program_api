579cd0ba3563aa6e4de6d1a4ff421eee81effbff
duxinghong <duxinghong@yinlimedia.com>
2024-11-08 16:16:19 +0800

    系统及官网相关

--- config/defaultdemo.go
+++ config/defaultdemo.go
@@ -5,9 +5,9 @@ const (
 //MYSQL_DEFAULT_DB = "root:asdf1234@tcp(127.0.0.1:3306)/dsp?charset=utf8mb4&parseTime=True&loc=Local"
 
 /*****redis*******/
-//REDIS_ADDRESS   = "139.224.40.69:6379"
-//REDIS_HOST      = "139.224.40.69"
-//REDIS_PASSWORD  = "Ylcm@NiRedis"
+//REDIS_ADDRESS   = "xxx.xx.xx.xx9:6379"
+//REDIS_HOST      = "xxx.xx.xx.xx"
+//REDIS_PASSWORD  = "xxx.xx.xx.xx"
 //REDIS_PORT      = 6379
 //REDIS_DB        = 3  // 数据库
 //REDIS_POOL_SIZE = 20 // 连接池大小
